df8afbcc33d85873a4e3caf9b85c60998785ec17
Sven Luzar <sven.luzar@fh-muenster.de>
2022-03-30 11:05:56 +0200

    init version
    Change-Id: I119a0f96f66f38a4107c1974c401c801d050fb15

---
+++ android/app/src/main/java/eu/fhms/lalo/MainActivity.java
@@ -0,0 +1,5 @@
+package eu.fhms.lalo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'eu.fhms.lalo',
+  appName: 'Lalo',
+  webDir: 'dist/alfabot',
+  bundledWebRuntime: false
+};
+
+export default config;
