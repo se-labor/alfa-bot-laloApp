8c347165339099cc93ff1e5e1df15631621c4bd7
Thomas Wagner <thomaswagner@fh-muenster.de>
2022-07-01 09:54:17 +0200

    Checked in test progress
    Change-Id: I0df6da9d8badb6ce300a420a3a1b530784a8e2b6

--- src/app/shared/services/message.service.spec.ts
+++ src/app/shared/services/message.service.spec.ts
@@ -1,17 +1,31 @@
-import {TestBed} from '@angular/core/testing';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import {MessageService} from './message.service';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {BotService} from "../../modules/api";
+
 
 describe('MessageService', () => {
   let service: MessageService;
+  let mockBotService: BotService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [{provide: BotService, use: mockBotService}]
     });
     service = TestBed.inject(MessageService);
 
+/*    mockBotService = TestBed.inject(BotService);
+    const responseObserver = function (observer: Observer<BotResponse[]>) {
+      observer.next([{
+        'message': '',
+        'imageUrl': '',
+        'buttons': [{'payload': 'TestButtonPayload', 'title': 'TestButtonTitle'}]}]);
+      observer.complete();
+    };
+    const responseObservable = new Observable(responseObserver);
+    spyOn(mockBotService, 'sendUserMessage').and.returnValue(responseObservable);*/
   });
 
   it('should be created', () => {
@@ -23,4 +37,10 @@ describe('MessageService', () => {
     expect(service.getMessages()).toContain(
       {identifier: '00000000-0000-0000-0000-000000000000', content: 'testMessage'});
   });
+
+  xit('should add the bot responses to the message list', fakeAsync(() => {
+    service.sendMessage({identifier: '00000000-0000-0000-0000-000000000000', content: 'testMessage'});
+    tick(300);
+    expect(service.getMessages().length).toBeGreaterThanOrEqual(2);
+  }));
 });
