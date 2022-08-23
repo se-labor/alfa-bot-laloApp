fe0874f0baf45ffe9631df0e999c808f125f4165
Thomas Wagner <thomaswagner@fh-muenster.de>
2022-08-23 13:59:13 +0200

    Implemeted automatic disabling of quick-response buttons after use.
    Change-Id: I4e7f4c7b0a4081359ec20f7b2ac053cf477e0508

--- src/app/chat/text-area/bot-response/bot-response.component.ts
+++ src/app/chat/text-area/bot-response/bot-response.component.ts
@@ -17,6 +17,7 @@ export class BotResponseComponent implements OnInit {
   public responseType;
   public showVoiceOutputButton;
   public responseTypeString: String;
+  public responseButtonsDisabled: boolean = false;
   constructor(private messageService: MessageService,
               private userService: UserService,
               private imageService: ImageService,
@@ -46,6 +47,7 @@ export class BotResponseComponent implements OnInit {
 
   onButtonClick(button: BotButton) {
     this.messageService.sendMessage({identifier: this.userService.getUUID(), content: button.title});
+    this.responseButtonsDisabled = true;
   }
 
   onImageLoad() {
