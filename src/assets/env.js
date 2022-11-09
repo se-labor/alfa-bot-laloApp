95268107613c6cff4bdc70841039697fca037979
Simon Proost <sp512339@fh-muenster.de>
2022-11-09 11:08:51 +0100

    Fixes for Launch
    * Add Icons
    * Remove LT-Wahl Bot from env.js
    * Add ngIF to display bot selection arrows only if more than one bot is available.
    
    Change-Id: I46fb9b495c1319eb0bcb6f17ffe49ea13b80ef2f

--- src/assets/env.js
+++ src/assets/env.js
@@ -11,13 +11,14 @@
         apiUrl: 'https://alfabot.se-labor.de/api-football-world-cup-2022',
         apiKey: '92d13306-b96e-11eb-b8e0-0242ac120002'
       },
-      {
+   /**   {
         id: 'wahlenNrw2022',
         name: 'Wahlen in NRW 2022',
         imageUrl: 'https://www.alfa-bot.de/wp-content/uploads/2022/08/wahlen_nrw_2022_header.png',
         apiUrl: 'https://alfabot.se-labor.de/alfabotapi',
         apiKey: '92d13306-b96e-11eb-b8e0-0242ac120002'
       }
+    **/
     ];
 
 
