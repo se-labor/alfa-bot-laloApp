7bcc6be104f7e36f56e046b4758086d5efea9952
Simon Proost <sp512339@fh-muenster.de>
2023-02-23 14:09:09 +0100

    Add new chatbot to selection and change settings description

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,24 +2,28 @@
   window.env = window.env || {};
   //Enviroment variables
 
-  window['env']['BOT_CONFIG'] =
+  window['env']['BOT_CONFIG']=
     [
       {
         id: 'fussballWm2022',
         name: 'Fußball-WM 2022',
-        imageUrl: 'https://www.alfa-bot.de/wp-content/uploads/2022/11/stadion.jpg',
+        imageUrl: 'https://www.alfa-bot.de/wp-content/uploads/2023/01/logo_fussball.png',
         apiUrl: 'https://alfabot.se-labor.de/api-football-world-cup-2022',
         apiKey: '92d13306-b96e-11eb-b8e0-0242ac120002'
       },
-   /**   {
+      {
         id: 'wahlenNrw2022',
-        name: 'Wahlen in NRW 2022',
-        imageUrl: 'https://www.alfa-bot.de/wp-content/uploads/2022/08/wahlen_nrw_2022_header.png',
+        name: 'Wahlen NRW 2022',
+        imageUrl: 'https://www.alfa-bot.de/wp-content/uploads/2023/01/logo_wahlen_nrw.png',
         apiUrl: 'https://alfabot.se-labor.de/alfabotapi',
         apiKey: '92d13306-b96e-11eb-b8e0-0242ac120002'
+      },
+      {
+        id: 'learningNuggets',
+        name: 'Lern-Häppchen',
+        imageUrl: 'https://www.alfa-bot.de/wp-content/uploads/2023/01/logo_lernhappchen.png',
+        apiUrl: 'https://alfabot.se-labor.de/api-learning-nuggets',
+        apiKey: '92d13306-b96e-11eb-b8e0-0242ac120002'
       }
-    **/
     ];
-
-
 })(this);
