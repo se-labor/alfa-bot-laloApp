df2c3d0c66d02795655079f559403a59702aa885
Thomas Wagner <thomaswagner@fh-muenster.de>
2023-07-07 15:25:41 +0200

    Refactored the Main menu
    Change-Id: I0b60af8a2fd80ca46466eda29c90d25a9bbc85f2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {ChatComponent} from './chat/chat.component';
 import {MenuHeaderComponent} from "./menu/menu-header/menu-header.component";
 import { SettingsComponent } from './menu/sub-menus/settings/settings.component';
 import { InfoComponent } from './menu/sub-menus/info/info.component';
+import {NgOptimizedImage} from "@angular/common";
 
 export function apiConfigFactory(): Configuration {
 
@@ -61,7 +62,8 @@ export function botServiceFactory(httpClient: HttpClient, basePath: string): Bot
     MaterialModule,
     FormsModule,
     MarkdownModule.forRoot({loader: HttpClient}),
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgOptimizedImage
   ],
   providers: [
     {
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,19 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {BotConfigService} from "../shared/services/bot-config.service";
 import {ChatBot} from "../shared/models/chat-bot.model";
 import {TextToSpeechService} from "../shared/services/text-to-speech.service";
+import KeenSlider, { KeenSliderInstance } from "keen-slider";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: [
+    '../../../node_modules/keen-slider/keen-slider.min.css',
+    './menu.component.scss']
 })
-export class MenuComponent {
-
-  public chatBots = [];
+export class MenuComponent implements AfterViewInit, OnDestroy{
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>
+  public chatBots:ChatBot[] = [];
   public activeChatBot: ChatBot;
 
   constructor(
@@ -18,17 +21,27 @@ export class MenuComponent {
     public textToSpeechService: TextToSpeechService
   ) {
     this.chatBots = botConfigService.chatBots;
-    this.activeChatBot = this.chatBots[0] ?? null;
   }
 
-  swapActiveChatBot(next: boolean) {
-    if (!this.activeChatBot) {
-      if(this.chatBots.length > 0) {
-        this.activeChatBot = this.chatBots[0];
-      }
-      return;
-    }
-    const index = this.chatBots.indexOf(this.activeChatBot);
-    this.activeChatBot = this.chatBots[(index + (next ? 1 : -1) + this.chatBots.length) % this.chatBots.length];
+  currentSlide: number = 1
+  dotHelper: Array<Number> = []
+  slider: KeenSliderInstance = null
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        initial: this.currentSlide,
+        slideChanged: (s) => {
+          this.currentSlide = s.track.details.rel
+        }, loop: true,
+      })
+      this.dotHelper = [
+        ...Array(this.slider.track.details.slides.length).keys(),
+      ]
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
   }
 }
