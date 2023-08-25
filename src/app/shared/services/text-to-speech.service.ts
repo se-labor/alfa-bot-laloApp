70f305cfcc56328dc1c88d16c0397857e3f51ea4
Thomas Wagner <thomaswagner@fh-muenster.de>
2023-08-25 10:04:18 +0200

    Fixed Conflicts and Cleaned up working-tree

--- src/app/shared/services/text-to-speech.service.ts
+++ src/app/shared/services/text-to-speech.service.ts
@@ -39,14 +39,18 @@ export class TextToSpeechService {
   filterText(text: string) {
     let filteredText: string = text;
     // Matches valid links, which are surrounded by brackets. Also includes matching of special german characters.
-    const rExp = new RegExp("\\(https?:\\/\\/(www\\.)?[\u00F0-\u02AF-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z\\d()]{1,6}\\b([\u00F0-\u02AF-a-zA-Z\\d!@:%_+.~#?&\\/=])+\\)");
+    const linkRExp = new RegExp("\\(https?:\\/\\/(www\\.)?[\u00F0-\u02AF-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z\\d()]{1,6}\\b([\u00F0-\u02AF-a-zA-Z\\d!@:%_+.~#?&\\/=])+\\)");
     // Removes link occurrences from the text, till there are none left
-    filteredText = filteredText.replace(rExp, "");
+    filteredText = filteredText.replace(linkRExp, "");
+    const telRExp = new RegExp("\\(tel:\\+?[0-9]{1,}\\)");
+    // Removes tel occurrences from the text, till there are none left
+    filteredText = filteredText.replace(telRExp, "");
     // Remove other unwanted symbols
     const symbolsToReplace = ["[", "]", "*"];
     symbolsToReplace.forEach((symbolToReplace) => {
       filteredText = filteredText.split(symbolToReplace).join("");
     });
+    console.log(filteredText);
     return filteredText;
   }
 
@@ -55,7 +59,10 @@ export class TextToSpeechService {
     const filteredText = this.filterText(text);
     if (filteredText !== '') {
       this.utterThis.text = filteredText; // Change the text of the utterance object to avoid creating a new one
-      await TextToSpeech.speak(this.utterThis)
+      try {
+        await TextToSpeech.speak(this.utterThis)
+      } catch (ignored) { // Ignore error, because it is thrown when the user stops the speech
+      }
     }
   }
 }
