5c01b6df4429d8251aa35560d04617631b5cfbf0
Thomas Wagner <thomaswagner@fh-muenster.de>
2023-08-31 09:04:29 +0200

    Added Tests for image-loaded.directive.ts

--- src/app/chat/text-area/bot-response/image-loaded.directive.spec.ts
+++ src/app/chat/text-area/bot-response/image-loaded.directive.spec.ts
@@ -1,1 +1,35 @@
 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ImageLoadedDirective } from './image-loaded.directive';
+
+@Component({
+  template: '<img (loaded)="onImageLoad()" appLoadedEmitter [src]="imageUrl" alt="">',
+})
+class TestComponent {
+  imageUrl = 'https://picsum.photos/200';
+  onImageLoad = jasmine.createSpy('onImageLoad');
+}
+
+describe('ImageLoadedDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, ImageLoadedDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should emit loaded event when image is loaded', () => {
+    const img = fixture.nativeElement.querySelector('img');
+    img.dispatchEvent(new Event('loaded'));
+    expect(component.onImageLoad).toHaveBeenCalled();
+  });
+});
