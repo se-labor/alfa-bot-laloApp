d32b6ab3d952414bdd726cd08fd2fb4e16584907
Thomas Wagner <thomaswagner@fh-muenster.de>
2023-09-14 15:17:17 +0200

    Fixed bugging Header and bugged chat view
    Change-Id: Ia8a6a55d508d4630309e730efa588137a9a7f2a1

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,24 +7,28 @@ import {AfterViewInit, Component, Input} from '@angular/core';
 })
 export class HeaderComponent implements AfterViewInit {
   @Input() title = "";
-  originalFontSize: number;
+  windowWidth: number;
+  private originalFontSize: number = 0;
 
   constructor() {
   }
 
   ngAfterViewInit() {
-    window.addEventListener('resize', this.adjustFontSize.bind(this));
-    const title = document.querySelector('.title');
-    if (title) {
-      this.originalFontSize = parseInt(
-        window.getComputedStyle(title).fontSize
-      );
-    }
     // Call after Load Event, because the Mat-Icon is not loaded before
     window.addEventListener('load', this.adjustFontSize.bind(this));
+    window.addEventListener('resize', this.adjustFontSize.bind(this));
+  }
+
+  setWindowWidth() {
+    this.windowWidth = window.innerWidth;
   }
 
   adjustFontSize() {
+    if (this.windowWidth === window.innerWidth) { // Catch vertical resize to prevent resizing when scrolling on ios
+      return;
+    }
+
+    this.setWindowWidth();
     const toolbar = document.querySelector('mat-toolbar');
     const title = document.querySelector('.title');
     const button = document.querySelector('.btn');
@@ -35,28 +39,31 @@ export class HeaderComponent implements AfterViewInit {
 
     let toolbarWidth = toolbar.getBoundingClientRect().width;
     const titleWidth = title.getBoundingClientRect().width;
-    const buttonWidth = title.getBoundingClientRect().width
-    let combinedWidth = titleWidth + buttonWidth + 16; //Add 16px for padding
+    const buttonWidth = button.getBoundingClientRect().width;
+    let combinedWidth = titleWidth + buttonWidth;
     let fontSize = parseInt(
       window.getComputedStyle(title).fontSize
     );
+    if (this.originalFontSize === 0) {
+      this.originalFontSize = fontSize;
+    }
+    console.log(toolbarWidth, titleWidth, buttonWidth, fontSize, this.originalFontSize);
 
-    if (combinedWidth > toolbarWidth) {
-      while (combinedWidth > toolbarWidth && fontSize > 0) {
-        fontSize--;
-        title.setAttribute('style', `font-size: ${fontSize}px`);
-        combinedWidth = title.getBoundingClientRect().width + buttonWidth;
-        toolbarWidth = toolbar.getBoundingClientRect().width;
-      }
-    } else if (combinedWidth + 10 < toolbarWidth) { // Add 10px to avoid jumping
-      if (fontSize < this.originalFontSize) {
-        fontSize++;
-        title.setAttribute('style', `font-size: ${fontSize}px`);
-      } else {
-        title.removeAttribute('style');
-        this.originalFontSize = parseInt(window.getComputedStyle(title).fontSize);
-      }
+    while (combinedWidth + 16 > toolbarWidth && fontSize > 0) {
+      fontSize--;
+      title.setAttribute('style', `font-size: ${fontSize}px`);
+      combinedWidth = title.getBoundingClientRect().width + buttonWidth;
+      toolbarWidth = toolbar.getBoundingClientRect().width;
+    }
+
+    while (combinedWidth + 30 < toolbarWidth && fontSize < this.originalFontSize) {
+      fontSize++;
+      title.setAttribute('style', `font-size: ${fontSize}px; line-height: ${fontSize}px`);
+      combinedWidth = title.getBoundingClientRect().width + buttonWidth;
+      toolbarWidth = toolbar.getBoundingClientRect().width;
+    }
+    if (fontSize >= this.originalFontSize) {
+      title.removeAttribute('style');
     }
   }
-
 }
